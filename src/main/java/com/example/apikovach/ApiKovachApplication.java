8c1656dea2b934342737606075c7fabba3dd9066
Kovach Volodymer <kovach.volodymyr1@student.uzhnu.edu.ua>
2024-07-19 16:29:56 +0300

    first commit

---
+++ src/main/java/com/example/apikovach/ApiKovachApplication.java
@@ -0,0 +1,15 @@
+package com.example.apikovach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+@SpringBootApplication
+public class ApiKovachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKovachApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/apikovach/ApiKovachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apikovach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKovachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
